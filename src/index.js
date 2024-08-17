478c8d30b5c3628a8987914f36fff3e35e2311db
Максим Жук <maxim.thudercat@gmail.com>
2024-08-17 20:49:23 +0300

    Completed Task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = Math.abs(n).toString();
+
+    let reversedStr = str.split("").reverse().join("");
+
+    let reversedNumber = parseInt(reversedStr, 10);
+
+    return reversedNumber;
+};
